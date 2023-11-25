0ef4f12b058015b8d5984c70c795c351f810e422
Sarun Kaewwongsa <k.sarun@odds.team>
2023-11-25 13:44:54 +0700

    decorator pattern (API)

--- code/src/decorator/lab/clientApi.test.ts
+++ code/src/decorator/lab/clientApi.test.ts
@@ -1,5 +1,7 @@
 import { Customer } from './customer';
 import { CustomerApi } from './decoratorApi/customerApi';
+import { CustomerWrappedApi } from './decoratorApi/customerWrapped';
+import { EncryptedCustomerApi } from './decoratorApi/encryptedCustomerApi';
 
 describe('[Decorator - lab] Build Response', () => {
   it('should build response', () => {
@@ -11,5 +13,61 @@ describe('[Decorator - lab] Build Response', () => {
 
     // then
     expect(customer.name).toBe('John');
+  });
+
+  it('should build response with wrapped status and data', () => {
+    //given
+    const api = new CustomerApi();
+    const wrappedApi = new CustomerWrappedApi(api);
+    const expectedData = {
+      status: 'ok',
+      data: {
+        customer: new Customer('John', 'Doe', 30),
+      },
+    };
+
+    //when
+    const result = wrappedApi.get();
+
+    //then
+    expect(result).toEqual(expectedData);
+  });
+
+  it('should return response with encrypted customer data', () => {
+    //given
+    const api = new CustomerApi();
+    const encryptedApi = new EncryptedCustomerApi(api);
+    const expectedData = {
+      name: 'encrypted John',
+      lastName: 'encrypted Doe',
+      age: 0,
+    };
+
+    //when
+    const result = encryptedApi.get();
+
+    //then
+    expect(result).toEqual(expectedData);
+  });
+
+  it('should return response with encrypted customer data wrapped with status and data', () => {
+    //given
+    const api = new CustomerApi();
+    const encryptedApi = new EncryptedCustomerApi(api);
+    const wrappedApi = new CustomerWrappedApi(encryptedApi);
+    const expectedData = {
+      status: 'ok',
+      data: {
+        name: 'encrypted John',
+        lastName: 'encrypted Doe',
+        age: 0,
+      },
+    };
+
+    //when
+    const result = wrappedApi.get();
+
+    //then
+    expect(result).toEqual(expectedData);
   });
 });
