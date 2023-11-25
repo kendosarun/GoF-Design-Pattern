15457b1761dc64fbfbfefe5dec449e9ba82e9a2e
Sarun Kaewwongsa <k.sarun@odds.team>
2023-11-25 14:17:20 +0700

    Singleon Pattern

--- code/src/singleton/simple/singleton.ts
+++ code/src/singleton/simple/singleton.ts
@@ -1,6 +1,7 @@
 export class Singleton {
   private static instance: Singleton;
   private constructor() {}
+
   public static getInstance(): Singleton {
     if (!Singleton.instance) {
       Singleton.instance = new Singleton();
